d0686d3e6e943f7769a8c23206d7d85dbcd3bf1e
Federico Dossena <info@fdossena.com>
2025-07-11 11:30:17 +0200

    Fixed typo

--- app.js
+++ app.js
@@ -795,7 +795,7 @@ function console_save() {
     }
     if (bowser.parse(navigator.userAgent).os.name === "Windows") {
         //yes, this is still a thing on windows... sigh
-        out.replace("\n", "\r\n")
+        out = out.replace("\n", "\r\n")
     }
     const blob = new Blob([out], {
         type: "text/plain",
